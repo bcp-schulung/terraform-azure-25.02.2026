2c19e005b37a6db7f2af976641ef2697752a3edd
Ben Cöppicus <ben.coeppicus@bencoepp.de>
2026-02-24 13:06:20 +0100

    Add backend configuration and provider setup for Azure in backend.tf

--- main.tf
+++ main.tf
@@ -1,18 +1,3 @@
-terraform {
-  required_version = ">= 1.5.0"
-
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.0"
-    }
-  }
-}
-
-provider "azurerm" {
-  features {}
-}
-
 data "azurerm_resource_group" "lab" {
   name = var.resource_group_name
 }
